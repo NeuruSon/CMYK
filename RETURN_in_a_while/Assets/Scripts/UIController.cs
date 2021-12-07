a18ea6aef16368e5c4098cff9fcaabf35043f4b8
NeuruSon <sonneuru@gmail.com>
2021-12-07 17:58:25 +0900

    delete_Title_sound+displayBtn function
    타이틀씬 설정에서 이제 사용하지 않는 버튼에 대한 상호작용 코드를 삭제

--- RETURN_in_a_while/Assets/Scripts/UIController.cs
+++ RETURN_in_a_while/Assets/Scripts/UIController.cs
@@ -8,13 +8,9 @@ public class UIController : MonoBehaviour
 {
     public GameObject UI;
     public bool active = false;
-    Button soundbtn; // display 버튼 클릭시 아래로 이동
-    Button displaybtn;
 
     private void Start()
     {
-        soundbtn = GameObject.Find("SoundSettings").GetComponent<Button>();
-        displaybtn = GameObject.Find("DisplaySettings").GetComponent<Button>();
     }
 
     public void OpenUI()
@@ -27,21 +23,7 @@ public class UIController : MonoBehaviour
         UI.SetActive(false);
     }
 
-    public void displayOpenAndCloseUI()
-    {
-        if (active == true)
-        {
-            soundbtn.transform.Translate(0, 300, 0);
-            UI.SetActive(false);
-            active = false;
-        }
-        else {
-            soundbtn.transform.Translate(0, -300, 0);
-            UI.SetActive(true);
-            active = true;
-        }
 
-    }
     public void OpenAndCloseUI()
     {
 
