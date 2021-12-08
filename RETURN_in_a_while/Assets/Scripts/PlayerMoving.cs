4e0a56d97944df7ad4a2dcfc47e2dfc13737f054
NeuruSon <sonneuru@gmail.com>
2021-12-08 19:51:44 +0900

    이동 관련 함수값 변경

--- RETURN_in_a_while/Assets/Scripts/PlayerMoving.cs
+++ RETURN_in_a_while/Assets/Scripts/PlayerMoving.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerMoving : MonoBehaviour
 {
     private Rigidbody r;
-    float rotateSpeed = 4f;
-    float jumpHeight = 5f;
     protected float speed = 5f; //이동 속도.
-    bool isJumping = true;
+    float rotateSpeed = 7f;
+    float jumpHeight = 9f;
+    public float moveSpeed;
+    bool isGround = false;
 
     public Camera pCam;
 
@@ -19,53 +20,63 @@ public class PlayerMoving : MonoBehaviour
 
     void Start()
     {
+        Physics.gravity = new Vector3(0, -12.8f, 0);
         r = GetComponent<Rigidbody>();
+        moveSpeed = speed;
     }
 
     void Update()
     {
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-1 * Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(-1 * Vector3.forward * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(-1 * Vector3.left * speed * Time.deltaTime);
+            transform.Translate(-1 * Vector3.left * moveSpeed * Time.deltaTime);
         }
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        //달리기
+        if (Input.GetKey(KeyCode.LeftShift) && isGround)
+        {
+            moveSpeed = 2 * speed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = speed;
+        }
+
+        //점프 
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            isGround = false;
+            r.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
 
         //마우스로 각도 회전 
         float X = Input.GetAxis("Mouse X") * rotateSpeed; //왼쪽 오른쪽 두리번 두리번 
-        float Y = Input.GetAxis("Mouse Y") * rotateSpeed; //위쪽 아래쪽 두리번 두리번 근데 너는 한 바퀴 돌면 안 됨 
-        transform.Rotate(0, X, 0); //왼쪽 오른쪽 두리번 두리번 
+        float Y = Input.GetAxis("Mouse Y") * rotateSpeed; //위쪽 아래쪽 두리번 두리번 근데 너는 한 바퀴 돌면 안 됨;
+        transform.Rotate(0, X, 0); //왼쪽 오른쪽 두리번 두리번
 
         if (pCam.transform.eulerAngles.x + (-Y) > 80 && pCam.transform.eulerAngles.x + (-Y) < 280)
             return; //조건만 만족하면 딱히 쓸 게 없음 
         else //모가지가 더 돌아가면 한정 범위 내 최솟값으로 바꿔부러라 
             pCam.transform.RotateAround(transform.position, pCam.transform.right, -Y); //RotateAround는 위치와 각도 모두 바꿔버리는데 위치 간섭을 제한하기 위해 transform.position(지금 위치) 그대로 설정하게 만듦.
-
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
-        {
-            isJumping = true;
-            r.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-        }
     }
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "ground")
         {
-            isJumping = false;
+            isGround = true;
         }
     }
 }
