7cd8917fb768928c04a96fa10dd0fc9b6432c3ae
NeuruSon <sonneuru@gmail.com>
2022-04-12 23:41:24 +0900

    continue_move

--- RETURN_in_a_while/Assets/Scripts/ContinueController.cs
+++ RETURN_in_a_while/Assets/Scripts/ContinueController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ContinueController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject player;
+    public bool isFollowing = true;
+    float targetDistance;
+    float allowedDistance = 3f;
+    float followSpeed;
+    RaycastHit ray;
+    Vector3 velocity = Vector3.zero;
+
     void Start()
     {
-        
+        player = GameObject.Find("Hero");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isFollowing)
+        {
+            transform.LookAt(player.transform);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out ray))
+            {
+                targetDistance = ray.distance;
+                if (targetDistance >= allowedDistance)
+                {
+                    followSpeed = 0.75f;
+                    transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref velocity, followSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                followSpeed = 0f;
+            }
+        }
     }
 }
