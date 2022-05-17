73680900f2007e9d23412d3d74fb9ba056762c9a
NeuruSon <sonneuru@gmail.com>
2022-05-17 18:46:11 +0900

    continue_flowchart

---
+++ RETURN_in_a_while/Assets/Scripts/GameSoundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSoundController : MonoBehaviour
+{
+    public AudioClip tutorial_bgm, flowchart_win_jignle;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void on_mainBGM()
+    {
+        audio.clip = tutorial_bgm;
+        audio.Play();
+    }
+}
